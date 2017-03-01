7f292aab0de5bb06fee2fdc9b6cdb2c05c771417
santosh <santosh.ece06@gmail.com>
2017-03-01 22:02:17 +0530

    angular lite server port change.

--- app/ts/main.ts
+++ app/ts/main.ts
@@ -4,4 +4,4 @@ import {AppComponent} from './app.component';
 import {AppComponent1} from './app.component1';
 import {AppComponent2} from './app.component2';
 
-bootstrap(AppComponent2);
+bootstrap(AppComponent);
